45fee5e3029e07f7fe4842a2d9ffae5a2ef95571
James Bain <jamescbain@gmail.com>
2019-02-28 09:14:34 -0600

    update with new components

--- app.R
+++ app.R
@@ -32,13 +32,18 @@ ui <- cartridge(
   ),
   cartridge(title = "flipity"),
   container(
+    imageOutput("flip_tracker_1"),
+    imageOutput("flip_tracker_2"),
+    imageOutput("flip_tracker_3")
+  ),
+  container(
     container(imageOutput("flip_image")),
     button_primary("flip", "flipity")
   )
 )
 
 server <- function(input, output, session) {
-  
+  # current flip count
   flip_count <- reactiveVal(1)
   
   # Actions that occur when flip button is pushed
@@ -51,7 +56,8 @@ server <- function(input, output, session) {
     flip()
   })
   
-  
+  # Generate a series of random flips
+  ## used in bar chart section
   tmp_dat <- reactive({
     tmp_flips <- crossing(trial = 1:input$pick_number, 
                           flip = 1:100) %>%
@@ -74,20 +80,45 @@ server <- function(input, output, session) {
       theme_bw()
     })
   
+  output$flip_tracker <- renderImage({
+    if (is.null(flip_event()))
+      return(NULL)
+    if (flip_event() %>% tail(1) %>% pull(heads) == 0) {
+      return(list(
+        src = "www/dogecoin-png-5.png",
+        filetype = "image/png",
+        width = 40,
+        height = 30,
+        alt = "heads"
+      ))
+    } else {
+      return(list(
+        src = "www/bitcoins.png",
+        filetype = "image/png",
+        width = 40,
+        height = 40,
+        alt = "tails"
+      ))
+    }}, deleteFile = FALSE)
+  
   output$flip_image <- renderImage({
     if (is.null(flip_event()))
       return(NULL)
     if (flip_event() %>% tail(1) %>% pull(heads) == 0) {
       return(list(
-        src = "face.png",
-        contentType = "image/png",
-        alt = "Face"
+        src = "www/dogecoin-png-5.png",
+        filetype = "image/png",
+        width = 40,
+        height = 30,
+        alt = "heads"
       ))
     } else {
       return(list(
-        src = "second.jpg",
-        filetype = "image/jpeg",
-        alt = "This is a chainring"
+        src = "www/bitcoins.png",
+        filetype = "image/png",
+        width = 40,
+        height = 40,
+        alt = "tails"
       ))
     }}, deleteFile = FALSE)
     
